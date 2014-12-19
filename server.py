1ab2fbec72ebf7c4a79dfe3959f17d7ecb6e238d
Errol Pineda <rrlqzdpnd@gmail.com>
2014-12-19 11:47:33 +0000

    can now register RFIDs

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 #! /usr/bin/python2.7
 
-import socket, threading
+import socket, threading, sqlite3
 
 class Server(object):
 	def __init__(self):
@@ -10,17 +10,41 @@ class Server(object):
 		
 	def listen(self):
 		while True:
+			self.sock.listen(1)
 			socket, address = self.sock.accept()
-			t = threading.Thread(target=communicate, args=(socket,))
+			print "Connected to", address[0]
+			t = threading.Thread(target=self.communicate, args=(socket,))
+			t.start()
 
 	def communicate(self, socket):
 		while True:
 			message = socket.recv(1024)
-			if message[:] == "":
-				pass
-		
-		
-	
+			if message[:6] == "[EXIT]":
+				print "Exiting"
+				break
+			elif message[:10] == "[REGISTER]":
+				params = message[11:].split("\t")
+				authstring = params[0]
+				lastname = params[1]
+				firstname = params[2]
+				
+				try:
+					conn = sqlite3.connect('userdatabase.db')
+					db = conn.cursor()
+					
+					db.execute("SELECT count(*) FROM users WHERE authstring = ?", (authstring,))
+					count = db.fetchall()
+					
+					if count[0][0] == 0:
+						db.execute("INSERT INTO users(lastname, firstname, authstring) VALUES (?, ?, ?)", (lastname, firstname, authstring))
+						conn.commit()
+						socket.send("[DONE]")
+					else:
+						print "Authstring collision"
+						socket.send("[RETRY]")
+				except Exception, socket.error:
+					socket.send("[FAIL]")
 if __name__ == "__main__":
 	server = Server()
 	server.listen()
+	
--- writeRFID.py
+++ writeRFID.py
@@ -1,44 +1,74 @@
 #! /usr/bin/python2.7
 
-import subprocess, string, random
+import subprocess, string, random, socket, time
 
 def generateAuthString(length):
 	return ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(length))
 
+def register(authstring, firstname, lastname):
+	try:
+		sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		sock.connect((raw_input("Enter IP address of server: "), 1625))
+		
+		authstringbuff = authstring
+		while True:
+			sock.send("[REGISTER]\t" + authstringbuff + "\t" + lastname + "\t" + firstname)
+			response = sock.recv(1024)
+			if response[:6] == "[DONE]":
+				break
+			elif response[:6] == "[FAIL]":
+				sock.send("[EXIT]")
+				sock.close()
+				return False
+				
+			authstringbuff = generateAuthString(16)
+		
+		sock.send("[EXIT]")
+		sock.close()
+		return True
+	except (Exception, socket.error) as e:
+		return False
+
 def main():
-	filein = raw_input("Enter file basis: ")
-	PIN = raw_input("Enter your PIN (must be 6 characters): ")
-	lastname = raw_input("Enter your last name: ")
-	firstname = raw_input("Enter your first name: ")
-	
-	if(len(PIN) != 6):
-		print "PIN must be 6 characters"
-		return
-	
-	fin = open(filein, "r")
-	fout = open("tmp.dmp", "w+b")
-	filecontents = fin.read()
-	authstring = generateAuthString(16)
-	for i in range(len(filecontents)):
-		byte = i % 16
-		block = (i / 16) % 4
-		sector = i / 64
+	try:
+		filein = raw_input("Enter file basis: ")
+		PIN = raw_input("Enter your PIN (must be 6 characters): ")
+		lastname = raw_input("Enter your last name: ")
+		firstname = raw_input("Enter your first name: ")
+		authstring = generateAuthString(16)
 		
-		if block == 1 and sector == 0:
-			fout.write(authstring[byte]) # write random generated auth string
-		elif sector == 0 and block == 3 and byte >= 10:
-			fout.write(PIN[byte-10]) # writes to sector 1, block 4, bytes 11 - 16
+		if(len(PIN) != 6):
+			print "PIN must be 6 characters"
+			return
+		
+		fin = open(filein, "r")
+		fout = open("tmp.dmp", "w+b")
+		filecontents = fin.read()
+		for i in range(len(filecontents)):
+			byte = i % 16
+			block = (i / 16) % 4
+			sector = i / 64
+			
+			if block == 1 and sector == 0:
+				fout.write(authstring[byte]) # write random generated auth string
+			elif sector == 0 and block == 3 and byte >= 10:
+				fout.write(PIN[byte-10]) # writes to sector 1, block 4, bytes 11 - 16
+			else:
+				fout.write(filecontents[i])
+			
+		fin.close()
+		fout.close()
+		
+		if register(authstring, firstname, lastname):
+			proc = subprocess.Popen(["nfc-mfclassic", "w", "A", "tmp.dmp", filein, "f"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+			(output, error) = proc.communicate()
+			print output
 		else:
-			fout.write(filecontents[i])
+			print "Something went wrong, possibly with connection to the server. Please try again later"
 		
-	fin.close()
-	fout.close()
-	
-	proc = subprocess.Popen(["nfc-mfclassic", "w", "A", "tmp.dmp", filein, "f"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-	(output, error) = proc.communicate()
-	print output
-	
-	subprocess.Popen(["rm", "tmp.dmp"])
+		subprocess.Popen(["rm", "tmp.dmp"])
+	except KeyboardInterrupt:
+		pass
 	
 if __name__ == "__main__":
 	main()
